06fcbd5c47e08e47184c18c4871a796714dbaf13
murad <muradkv@gmail.com>
2019-03-19 22:12:10 +0300

    Add IBOutlet and IBActions to ChangeCityViewController

--- Clima/Controller/ChangeCityViewController.swift
+++ Clima/Controller/ChangeCityViewController.swift
@@ -10,4 +10,12 @@ import UIKit
 
 class ChangeCityViewController: UIViewController {
     
+    @IBOutlet weak var changeCityTextField: UITextField!
+    
+    @IBAction func getWeatherPressed(_ sender: Any) {
+    }
+    
+    @IBAction func backButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
 }
