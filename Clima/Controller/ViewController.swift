295f28af7c95d93a2eed68178e31bdf17fd98eee
murad <muradkv@gmail.com>
2024-10-18 04:19:10 +0300

    Initial commit

---
+++ Clima/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by murad on 18.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .brown
+    }
+
+
+}
+
